f259ba3b1f8aea9f4bdd767060d63a02be6f65bf
Caleb Woodbine <calebwoodbine.public@gmail.com>
2024-03-04 11:31:23 +1300

    chore: use locals for common configurations
    specify once

--- main.tf
+++ main.tf
@@ -1,14 +1,21 @@
+locals {
+  ipxe_script_url    = "https://pxe.factory.talos.dev/pxe/3c34c887de9c7013d7b382f3796d7465d7a5f54b2757e1a50d663ed7a303385d/v1.6.5/metal-amd64"
+  talos_version      = "v1.6.5"
+  kubernetes_version = "v1.29.2"
+  metro              = "sv"
+}
+
 module "cloudnative-coop" {
   source = "./terraform/equinix-metal-talos-cluster"
 
   cluster_name             = "cloudnative-coop"
   kube_apiserver_domain    = "k8s.sharing.io"
   equinix_metal_project_id = var.equinix_metal_project_id
-  equinix_metal_metro      = "sv"
+  equinix_metal_metro      = local.metro
   equinix_metal_auth_token = var.equinix_metal_auth_token
-  talos_version            = "v1.6.5"
-  kubernetes_version       = "v1.29.2"
-  ipxe_script_url          = "https://pxe.factory.talos.dev/pxe/3c34c887de9c7013d7b382f3796d7465d7a5f54b2757e1a50d663ed7a303385d/v1.6.5/metal-amd64"
+  talos_version            = local.talos_version
+  kubernetes_version       = local.kubernetes_version
+  ipxe_script_url          = local.ipxe_script_url
   controlplane_nodes       = 3
 
   providers = {
@@ -37,11 +44,11 @@ module "sharing-io" {
   cluster_name             = "sharing-io"
   kube_apiserver_domain    = "sharingio.sharing.io"
   equinix_metal_project_id = var.equinix_metal_project_id
-  equinix_metal_metro      = "sv"
+  equinix_metal_metro      = local.metro
   equinix_metal_auth_token = var.equinix_metal_auth_token
-  talos_version            = "v1.6.5"
-  kubernetes_version       = "v1.29.2"
-  ipxe_script_url          = "https://pxe.factory.talos.dev/pxe/3c34c887de9c7013d7b382f3796d7465d7a5f54b2757e1a50d663ed7a303385d/v1.6.5/metal-amd64"
+  talos_version            = local.talos_version
+  kubernetes_version       = local.kubernetes_version
+  ipxe_script_url          = local.ipxe_script_url
   controlplane_nodes       = 3
 
   providers = {
