01dc4a5319a23a4667ca00ed9469e0636f0dcdd2
Caleb Woodbine <calebwoodbine.public@gmail.com>
2024-03-05 09:08:50 +1300

    chore: remove outputs
    unused

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,3 @@
-output "cloudnative-coop-talosconfig" {
-  value     = module.cloudnative-coop.talosconfig
-  sensitive = true
-}
-
-output "cloudnative-coop-kubeconfig" {
-  value     = module.cloudnative-coop.kubeconfig
-  sensitive = true
-}
-
-output "cloudnative-coop-cluster-virtual-ip" {
-  value = module.cloudnative-coop.cluster_virtual_ip
-}
-
 output "sharing-io-talosconfig" {
   value     = module.sharing-io.talosconfig
   sensitive = true
