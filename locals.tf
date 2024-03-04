6547c530d3cf223661d45edb7e8c68b8023b0f92
Caleb Woodbine <calebwoodbine.public@gmail.com>
2024-03-05 09:09:33 +1300

    chore move locals into locals.tf
    spread out

---
+++ locals.tf
@@ -0,0 +1,6 @@
+locals {
+  ipxe_script_url    = "https://pxe.factory.talos.dev/pxe/3c34c887de9c7013d7b382f3796d7465d7a5f54b2757e1a50d663ed7a303385d/v1.6.5/metal-amd64"
+  talos_version      = "v1.6.5"
+  kubernetes_version = "v1.29.2"
+  metro              = "sv"
+}
--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-locals {
-  ipxe_script_url    = "https://pxe.factory.talos.dev/pxe/3c34c887de9c7013d7b382f3796d7465d7a5f54b2757e1a50d663ed7a303385d/v1.6.5/metal-amd64"
-  talos_version      = "v1.6.5"
-  kubernetes_version = "v1.29.2"
-  metro              = "sv"
-}
-
 module "sharing-io" {
   source = "./terraform/equinix-metal-talos-cluster"
 
