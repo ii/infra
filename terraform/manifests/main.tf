3480c141871ac47c79ca9d4c357fbdbcddf77716
Hippie Hacker <hh@ii.coop>
2024-03-10 01:08:38 -0700

    Remove most of the manifests/main.tf
    Place configuration in separate files per area

--- terraform/manifests/main.tf
+++ terraform/manifests/main.tf
@@ -1,54 +1,24 @@
-resource "kubernetes_namespace" "kube-system" {
-  metadata {
-    name = "kube-system"
-    labels = {
-      "pod-security.kubernetes.io/enforce" = "privileged"
-    }
-  }
+# NOTE:
+# module.sharing-io-manifests.kubernetes_namespace.kube-system: Creating...
+#╷
+#│ Error: namespaces "kube-system" already exists
+#│
+#│   with module.sharing-io-manifests.kubernetes_namespace.kube-system,
+#│   on terraform/manifests/main.tf line 1, in resource "kubernetes_namespace" "kube-system":
+#│    1: resource "kubernetes_namespace" "kube-system" {
+#│
+# resource "kubernetes_namespace" "kube-system" {
+#   metadata {
+#     name = "kube-system"
+#     labels = {
+#       "pod-security.kubernetes.io/enforce" = "privileged"
+#     }
+#   }
 
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "kubernetes_namespace" "flux-system" {
-  metadata {
-    name = "flux-system"
-  }
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes = [
-      metadata["labels"],
-    ]
-  }
-}
-
-resource "kubernetes_namespace" "cert-manager" {
-  metadata {
-    name = "cert-manager"
-  }
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes = [
-      metadata["labels"],
-    ]
-  }
-}
-
-resource "kubernetes_namespace" "authentik" {
-  metadata {
-    name = "authentik"
-  }
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes = [
-      metadata["labels"],
-    ]
-  }
-}
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
 resource "kubernetes_config_map_v1" "ingress-ip" {
   metadata {
@@ -59,102 +29,7 @@ resource "kubernetes_config_map_v1" "ingress-ip" {
   data = {
     ingressip = var.ingress_ip
   }
-}
-
-resource "kubernetes_secret_v1" "rfc2136-dns-server" {
-  metadata {
-    name      = "rfc2136dnsserver"
-    namespace = "flux-system"
-  }
-
-  data = {
-    email      = var.acme_email_address
-    nameserver = var.rfc2136_nameserver
-    keyname    = var.rfc2136_tsig_keyname
-    key        = var.rfc2136_tsig_key
-    algorithm  = var.rfc2136_algorithm
-    domain     = var.domain
-    pdnsapikey = var.pdns_api_key
-    pdnshost   = var.pdns_host
-  }
-}
-
-resource "kubernetes_secret_v1" "rfc2136-cert-manager" {
-  metadata {
-    name      = "rfc2136"
-    namespace = "cert-manager"
-  }
-
-  data = {
-    key = var.rfc2136_tsig_key
-  }
-}
-
-resource "kubernetes_secret_v1" "pdns-cert-manager" {
-  metadata {
-    name      = "pdns"
-    namespace = "cert-manager"
-  }
-
-  data = {
-    api-key = var.pdns_api_key
-  }
-}
-
-# TODO move outside the manifest module
-resource "random_string" "flux_receiver_token" {
-  length  = 12
-  special = false
-  lower   = true
-  upper   = false
-  numeric = false
-}
-
-resource "kubernetes_secret_v1" "flux_receiver_token" {
-  metadata {
-    name      = "receiver-token"
-    namespace = "flux-system"
-  }
-
-  data = {
-    token = random_string.flux_receiver_token.result
-  }
-}
-
-# TODO move outside the manifest module
-resource "random_string" "authentik_bootstrap_password" {
-  length  = 16
-  special = false
-  lower   = true
-  upper   = false
-  numeric = false
-}
-# TODO move outside the manifest module
-resource "random_string" "authentik_bootstrap_token" {
-  length  = 16
-  special = false
-  lower   = true
-  upper   = false
-  numeric = false
-}
-# TODO move outside the manifest module
-resource "random_string" "authentik_secret_key" {
-  length  = 50
-  special = false
-  lower   = true
-  upper   = false
-  numeric = false
-}
-
-resource "kubernetes_secret_v1" "authentik_env" {
-  metadata {
-    name      = "authentik-env"
-    namespace = "authentik"
-  }
-
-  data = {
-    AUTHENTIK_BOOTSTRAP_PASSWORD = random_string.authentik_bootstrap_password.result
-    AUTHENTIK_BOOTSTRAP_TOKEN    = random_string.authentik_bootstrap_token.result
-    AUTHENTIK_SECRET_KEY         = random_string.authentik_secret_key.result
-  }
+  depends_on = [
+    kubernetes_namespace.flux-system
+  ]
 }
