821235078e43e5c3bbae55400312ee655ea147b6
Caleb Woodbine <calebwoodbine.public@gmail.com>
2024-03-12 11:58:30 +1300

    chore: add most of powerdns values
    add values for configuring powerdns and pdns-admin

--- terraform/manifests/powerdns.tf
+++ terraform/manifests/powerdns.tf
@@ -33,8 +33,20 @@ resource "kubernetes_secret_v1" "powerdns-config" {
   }
 
   data = {
-    PDNS_AUTH_API_KEY   = random_string.auth_api_key.result
-    PDNS_gpsql_password = random_string.gpsql_password.result
+    PDNS_AUTH_API_KEY       = random_string.auth_api_key.result
+    PDNS_gpsql_password     = random_string.gpsql_password.result
+    SQLALCHEMY_DATABASE_URI = ""
+    SECRET_KEY              = random_string.auth_api_key.result
+    PDNS_SITE_NAME          = "PowerDNS"
+    PDNS_URL                = "https://pdns.${var.domain}"
+    PDNS_VERSION            = "4.8.1"
+    # TODO correct this value?
+    PDNS_ZONES          = <<-EOT
+      ${var.domain}
+EOT
+    PDNS_ADMIN_USER     = ""
+    PDNS_ADMIN_PASSWORD = ""
+    PDNS_ADMIN_EMAIL    = ""
   }
   depends_on = [
     kubernetes_namespace.powerdns
@@ -48,12 +60,18 @@ resource "kubernetes_config_map_v1" "powerdns-config" {
   }
 
   data = {
-    PRIMARY_DOMAIN    = var.domain
-    TEMPLATE_FILES    = "_api,gpsql,dnsupdate,soa-content"
-    PDNS_gpsql_dnssec = "yes"
-    PDNS_gpsql_host   = "pdns-db-postgresql"
-    PDNS_gpsql_dbname = "postgres"
-    PDNS_SITE_NAME    = "PowerDNS for ${var.domain}"
+    PRIMARY_DOMAIN            = var.domain
+    TEMPLATE_FILES            = "_api,gpsql,dnsupdate,soa-content"
+    PDNS_gpsql_dnssec         = "yes"
+    PDNS_gpsql_host           = "pdns-db-postgresql"
+    PDNS_gpsql_dbname         = "postgres"
+    PDNS_SITE_NAME            = "PowerDNS for ${var.domain}"
+    SQLALCHEMY_ENGINE_OPTIONS = <<-EOT
+                {
+                  "echo": "True"
+                  "echo_pool": "True"
+                }
+EOT
   }
   depends_on = [
     kubernetes_namespace.cert-manager
